8c859bbab0c3766428dee5078291a4fff23626c6
Abhishek <dhaneshwarabhishek@gmail.com>
2025-11-02 18:38:56 +0530

    test java project added

--- README.md
+++
@@ -1,4 +0,0 @@
-# TestGit
-My Git Test Project
-Lets learn git 
-added line testing the code
---
+++ learncicd/src/main/java/com/example/learncicd/LearncicdApplication.java
@@ -0,0 +1,14 @@
+package com.example.learncicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearncicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearncicdApplication.class, args);
+		System.out.println("Hey I am here");
+	}
+
+}
