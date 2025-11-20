df3c6389575f5e909590cf2fa73b75e3507c41bf
Abhishek <dhaneshwarabhishek@gmail.com>
2025-11-20 14:48:10 +0530

    commit all

--- learncicd/src/main/java/com/example/learncicd/LearncicdApplication.java
+++ learncicd/src/main/java/com/example/learncicd/LearncicdApplication.java
@@ -9,6 +9,8 @@ public class LearncicdApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LearncicdApplication.class, args);
 		System.out.println("Hey I am here");
+		System.out.println("Hey I m again");
+		
 	}
 
 }
