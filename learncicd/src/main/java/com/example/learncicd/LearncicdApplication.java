6c8bafb4e0bc94e6b0416b69fae2d627790e2902
Abhishek <dhaneshwarabhishek@gmail.com>
2025-11-20 15:20:17 +0530

    added a line

--- learncicd/src/main/java/com/example/learncicd/LearncicdApplication.java
+++ learncicd/src/main/java/com/example/learncicd/LearncicdApplication.java
@@ -9,7 +9,7 @@ public class LearncicdApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LearncicdApplication.class, args);
 		System.out.println("Hey I am here");
-		System.out.println("Hey I m again");
+		System.out.println("Hey I m again, trying codeing git");
 		
 	}
 
